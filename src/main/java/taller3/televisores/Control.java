c64f9148fa603d2be2ea5a7ad8e716553ea3385d
jumartineze <55594407+jumartineze@users.noreply.github.com>
2021-11-01 19:39:00 -0500

    Add files via upload

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,42 @@
+package televisores;
+
+public class Control {
+	private TV tv;
+	
+	public void turnOn() {
+		tv.turnOn();
+	}
+	public void turnOff() {
+		tv.turnOff();
+	}
+	
+	public void canalUp() {
+		tv.canalUp();
+	}
+	public void canalDown() {
+		tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		tv.volumenUp();
+	}
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+	
+	public void enlazar(TV televisor) {
+		tv = televisor;
+		tv.setControl(this);
+	}
+	
+	public TV getTv() {
+		return tv;
+	}
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+}
